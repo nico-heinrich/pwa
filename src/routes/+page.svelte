f2506ea85429f40bcb98bec8084a00e5ee520424
nico-heinrich <heinrich.nico@gmail.com>
2024-07-06 17:57:50 +0200

    test save area

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="h-screen flex flex-col bg-red-500">
+  <h1>Welcome to SvelteKit</h1>
+  <p class="mt-auto mb-save-area">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</div>
+
+<style>
+  .mb-save-area {
+    margin-bottom: env(safe-area-inset-bottom);
+  }
+</style>
